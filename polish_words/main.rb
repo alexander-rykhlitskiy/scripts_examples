636479f515eb09dc82ccf9dc1c6719f685363358
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2021-06-23 18:40:53 +0300

    Generate anki cards

--- polish_words/main.rb
+++ polish_words/main.rb
@@ -3,19 +3,29 @@ require 'uri'
 require 'cgi'
 require 'nokogiri'
 require 'fileutils'
+require 'csv'
 
 CSV_DIR = ARGV[0]
 raise 'No dir with CSV files provided' unless CSV_DIR
 
-ALL_WORDS_CSV_FILE_NAME = 'words.csv'
-`cat #{CSV_DIR}/* > #{ALL_WORDS_CSV_FILE_NAME}`
+ANKI = !!ARGV[1]
 
-words = File.read(ALL_WORDS_CSV_FILE_NAME).scan(/([^\s",\d]+ć),/).flatten.uniq.sort
-
-FileUtils.mkdir_p('html')
+def read_words
+  rows = Dir["#{CSV_DIR}/*"]
+    .sort_by { _1.split('/').last.to_i }.first(14)
+    .flat_map { |file| CSV.read(file) rescue puts "Error reading csv #{file}" }.compact
+  rows.uniq! do |row|
+    if row[0]
+      row[0] = row[0].split(',').first
+      row[0].gsub!('(się)', 'się')
+      row[0]
+    end
+  end
+  rows.select { |row| row[0]&.match?(/ć(\z|\s)/) }
+end
 
 def word_url(word)
-  "https://en.bab.la/conjugation/polish/#{CGI.escape(word)}"
+  "https://en.bab.la/conjugation/polish/#{CGI.escape(word).gsub('+', '%20')}"
 end
 
 def fetch(word)
@@ -24,41 +34,45 @@ def fetch(word)
     File.read(path)
   else
     result = begin
+      puts "Fetching #{word}"
       URI.open(word_url(word)).read
     rescue OpenURI::HTTPError
       puts "#{word} not found"
       ''
     end
+    FileUtils.mkdir_p('html')
     File.write(path, result)
     result
   end
 end
 
-body_html = ''
-
-words.shuffle.each do |word|
-  puts "Fetching #{word}"
+def fetch_tenses(word)
   page_html = fetch(word)
-  next if page_html.empty?
+  return if page_html.empty?
 
-  page = Nokogiri(page_html)
-  tenses = page.css('.conj-tense-block')
-  body_html += <<~HTML
-  <div>
-    <h2><a href="#{word_url(word)}" target="_blank">#{word}</a></h2>
-    <div>
-      #{tenses[0].to_s}
-      #{tenses[1].to_s}
-    </div>
-  </div>
+  page = Nokogiri(page_html.gsub('on/ona/ono', 'on(a,o)'))
+  page.css('.conj-tense-block')
+end
+
+def write_file(file_name, content)
+  result_dir = 'public'
+  FileUtils.mkdir_p(result_dir)
+  result_full_name = File.join(result_dir, file_name)
+  File.write(result_full_name, content)
+end
+
+def word_wiki_url(word)
+  "https://pl.wiktionary.org/wiki/#{word}"
+end
+
+def word_link(word, text = nil)
+  <<~HTML
+    <a href="#{word_wiki_url(word)}" target="_blank">#{text || word}</a>
   HTML
 end
 
-result_html = <<~HTML
-<html>
-  <head>
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+def styles
+  <<~HTML
     <style>
       .conj-person {
         margin-right: 10px;
@@ -67,18 +81,60 @@ result_html = <<~HTML
         width: 20%;
         text-align: right;
       }
-      .conj-person, .conj-result {
-        display: inline;
+      .conj-result {
+        text-align: left;
       }
     </style>
-  </head>
-  <body>#{body_html}</body>
-</html>
-HTML
+  HTML
+end
 
-result_file_name = 'index.html'
-result_dir = 'public'
-FileUtils.mkdir_p(result_dir)
-result_full_name = File.join(result_dir, result_file_name)
-File.write(result_full_name, result_html)
-`google-chrome #{result_full_name}`
+def create_anki_file
+  result = read_words.map do |word, translation|
+    tenses = fetch_tenses(word) || next
+    imperative = tenses.find { _1.to_s.include?('Tryb rozkazujący') }
+
+    [
+      word,
+      "#{styles}<div><div>#{word_link(word, translation)}</div><br>#{tenses[0]} <br>#{tenses[1]} <br>#{imperative}<br></div>"
+    ].map { "\"#{_1.gsub('"', '""')}\"" }.join("\t").gsub(/[\r\n]+/, '')
+  end.compact.join("\n")
+  write_file('anki.txt', result)
+end
+
+def create_html_file
+  body_html = ''
+
+  read_words.shuffle.each do |word, translation|
+    tenses = fetch_tenses || next
+
+    body_html += <<~HTML
+    <div>
+      <h2>word_link(word)</h2>
+      <div>
+        #{tenses[0].to_s}
+        #{tenses[1].to_s}
+      </div>
+    </div>
+    HTML
+  end
+
+  result_html = <<~HTML
+  <html>
+    <head>
+      <meta content="width=device-width, initial-scale=1" name="viewport">
+      <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+      #{styles}
+    </head>
+    <body>#{body_html}</body>
+  </html>
+  HTML
+  write_file('index.html', result_html)
+  `google-chrome #{result_full_name}`
+end
+
+if ANKI
+  create_anki_file
+else
+  create_html_file
+end
+
